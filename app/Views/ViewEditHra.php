64edde322dd2f0cd66681d97886e80334fc48f59
xdole <vasikdolezal2006@gmail.com>
2025-06-12 21:57:29 +0200

    EditHra

--- app/Views/ViewEditHra.php
+++ app/Views/ViewEditHra.php
@@ -4,30 +4,30 @@
 <!-- Edit Game Form -->
 <div class="bg-white shadow-lg editdiv">
     <div class="m-3">
-        <h1 class="">Edit Game</h1>
+        <h1 class="">Uprav hru</h1>
 
         <form action="<?= site_url('hry/update/' . $hra->id) ?>" method="post">
             <!-- Title Field -->
             <div class="mb-4 ">
-                <label class="block font-medium mb-1">Title</label>
-                <input type="text" name="title" value="<?= $hra->title ?>" class="w-full border rounded px-3 py-2">
+                <label class="block font-medium mb-1">Název</label>
+                <input type="text" name="nazev" value="<?= $hra->nazev ?>" class="w-full border rounded px-3 py-2">
             </div>
 
             <!-- Description Field -->
             <div class="mb-4">
-                <label class="block font-medium mb-1">Description</label>
-                <textarea id="description" name="description"><?= $hra->description ?></textarea>
+                <label class="block font-medium mb-1">Popis</label>
+                <textarea id="popis" name="popis"><?= $hra->popis ?></textarea>
             </div>
 
             <!-- Published Date Field -->
             <div class="mb-4">
-                <label class="block font-medium mb-1">Published Date</label>
-                <input type="text" name="published_date" value="<?= $hra->published_date ?>" class="w-full border rounded px-3 py-2">
+                <label class="block font-medium mb-1">Datum vydání</label>
+                <input type="text" name="datum_vydani" value="<?= $hra->datum_vydani ?>" class="w-full border rounded px-3 py-2">
             </div>
 
                <!-- Submit Button -->
             <div class="text-right mb-4">
-                <button type="submit" class="px-4 py-2 rounded savechanges">Save Changes</button>
+                <button type="submit" class="px-4 py-2 rounded savechanges">Ulož změny</button>
             </div>
         </form>
     </div>
